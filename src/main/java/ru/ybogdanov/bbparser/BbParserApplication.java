fe8202e75770f496f78832fc85c77ca951901a07
ybogdanov <dante4645@yandex.ru>
2021-06-21 17:33:46 +0400

    Initial commit

---
+++ src/main/java/ru/ybogdanov/bbparser/BbParserApplication.java
@@ -0,0 +1,13 @@
+package ru.ybogdanov.bbparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ybogdanov/bbparser/BbParserApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ybogdanov.bbparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
